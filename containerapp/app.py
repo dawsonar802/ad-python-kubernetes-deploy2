2c521c71c27d16ba185584e9d308e1bb43b0730c
Andrew Dawson <62777308+dawsonar802@users.noreply.github.com>
2021-05-21 12:45:56 -0400

    Update app.py

--- containerapp/app.py
+++ containerapp/app.py
@@ -3,10 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/pups', methods=['GET'])
+@app.route('/frogs', methods=['GET'])
 
 def pups():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run()
